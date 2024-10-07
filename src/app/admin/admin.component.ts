273dbb3c3ae4cf40d144504a287bb61c2e5e9acb
taras.sokha <taras.sokha@nerdysoft.com>
2024-10-07 20:13:51 +0300

    lint fixes

--- src/app/admin/admin.component.ts
+++ src/app/admin/admin.component.ts
@@ -1,48 +1,40 @@
 import { Component } from '@angular/core';
-import {AddTourTypeComponent} from '../dialog/admin/add-tour-type/add-tour-type.component';
-import {MatDialog} from '@angular/material';
-import {AddCountryComponent} from '../dialog/admin/add-country/add-country.component';
-import {AddHotelComponent} from '../dialog/admin/add-hotel/add-hotel.component';
-import {AddTourComponent} from '../dialog/admin/add-tour/add-tour.component';
+import { MatDialog } from '@angular/material';
+
+import { AddCountryComponent } from '../dialog/admin/add-country/add-country.component';
+import { AddHotelComponent } from '../dialog/admin/add-hotel/add-hotel.component';
+import { AddTourTypeComponent } from '../dialog/admin/add-tour-type/add-tour-type.component';
+import { AddTourComponent } from '../dialog/admin/add-tour/add-tour.component';
 
 @Component({
   selector: 'app-admin',
+  styleUrls: ['./admin.component.css'],
   templateUrl: './admin.component.html',
-  styleUrls: ['./admin.component.css']
 })
 export class AdminComponent {
-
   constructor(public dialog: MatDialog) {}
 
-  openAddTourTypeDialog(): void {
-    const dialogRef = this.dialog.open(AddTourTypeComponent, {
-    });
+  public openAddTourTypeDialog(): void {
+    const dialogRef = this.dialog.open(AddTourTypeComponent, {});
 
-    dialogRef.afterClosed().subscribe(result => {
-    });
+    dialogRef.afterClosed().subscribe((result) => {});
   }
 
-  openAddTourDataDialog(): void {
-    const dialogRef = this.dialog.open(AddTourComponent, {
-    });
+  public openAddTourDataDialog(): void {
+    const dialogRef = this.dialog.open(AddTourComponent, {});
 
-    dialogRef.afterClosed().subscribe(result => {
-    });
+    dialogRef.afterClosed().subscribe((result) => {});
   }
 
-  openAddCountryDialog(): void {
-    const dialogRef = this.dialog.open(AddCountryComponent, {
-    });
+  public openAddCountryDialog(): void {
+    const dialogRef = this.dialog.open(AddCountryComponent, {});
 
-    dialogRef.afterClosed().subscribe(result => {
-    });
+    dialogRef.afterClosed().subscribe((result) => {});
   }
 
-  openAddHotelDialog(): void {
-    const dialogRef = this.dialog.open(AddHotelComponent, {
-    });
+  public openAddHotelDialog(): void {
+    const dialogRef = this.dialog.open(AddHotelComponent, {});
 
-    dialogRef.afterClosed().subscribe(result => {
-    });
+    dialogRef.afterClosed().subscribe((result) => {});
   }
 }
